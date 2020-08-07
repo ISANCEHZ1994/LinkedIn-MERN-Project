494173917cdc006e84aa2ac30f5d9ef58acdcbc6
Israel <Israel.pro1994@gmail.com>
2020-08-07 18:35:41 -0500

    working on frontend

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,25 +1,62 @@
 import React from 'react';
+import Axios from 'axios';
 import PlayerList from '../Components/Players/PlayerList';
 import PlayerForm from '../Components/Players/PlayerForm';
 import PlayerSingle from '../Components/Players/PlayerSingle';
 
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      players: [],
+      currentPlayer: {}
+    }
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+  }
+
+  componentDidMount(){
+    const url = 'http://localhost:3001/players';
+    Axios.get(url)
+    .then( resp => {
+      this.setState({
+        players: resp.data
+      })
+    })
+    .catch((error => {
+      console.log(error)
+    }))
+  }
+
+  updateCurrentPlayer(item) {
+    this.setState({
+      currentPlayer: item
+    })
+  }
+
+  render(){
+
+    return (
    <div className='container-fluid'>
       <div className="row">
         <div className="col s12">Menu</div> {/* 12 is the number of spaces you can use for a particular area*/}
       </div>
       <div className="row">
-        <div className="col s3"><PlayerList/></div>
-        <div className="col s9"><PlayerSingle/></div>
+        <div className="col s3">
+          <PlayerList 
+          players={this.state.players} 
+          updateCurrentPlayer={this.updateCurrentPlayer}
+          />
+        </div>
+        <div className="col s9"><PlayerSingle player={this.state.currentPlayer}/></div>
       </div>
       <div className="row">
         <div className="col s12"><PlayerForm/></div>
       </div>
    </div>
-  );
-}
+  )};
+};
 
 export default App;
--- frontend/src/Components/Players/PlayerList.js
+++ frontend/src/Components/Players/PlayerList.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 
-const PlayerList = () => {
+const PlayerList = (props) => {
     return(
         <div>
-           this is the player list
+            {console.log(props)}
+           <ul className='collection with-header'>
+               <li className='collection-header'>
+                    <h4>Players</h4>  
+               </li>
+               {props.players.map( player => (
+                   <a href='#!' 
+                   className='collection-item' 
+                   key={player.id}
+                   onClick={props.updateCurrentPlayer.bind(this,player)}
+                   > 
+                   {player.firstName}
+                   {player.lastName}
+                   </a>
+               ))}
+           </ul>
         </div>
     );
 }
